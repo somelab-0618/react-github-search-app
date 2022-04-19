0df01a5442e7a8225b0b181ea6fc5ea5341c38c5
RyosukeSomeya <rx.0.0618@gmail.com>
2022-04-20 01:40:44 +0900

    GithubTokenをdotenvファイルに設定

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-function App() {
+import React from 'react';
+
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+console.log(GITHUB_TOKEN);
+
+const App = () => {
   return <div>GraphQL</div>;
-}
+};
 
 export default App;
