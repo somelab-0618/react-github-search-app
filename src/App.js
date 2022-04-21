f0ddb14fc0ea4f72a9b7d189cd1bc9afb2d3be19
RyosukeSomeya <rx.0.0618@gmail.com>
2022-04-22 01:00:04 +0900

    検索結果タイトル表示を追加

--- src/App.js
+++ src/App.js
@@ -30,8 +30,13 @@ const App = () => {
         {({ loading, error, data }) => {
           if (loading) return 'loading...';
           if (error) return `Error ${error.message}`;
-          console.log({ data });
-          return <div></div>;
+
+          const search = data.search;
+          const repositoryCount = search.repositoryCount;
+          const repositoryUnit =
+            repositoryCount === 1 ? 'Repository' : 'Repositories';
+          const title = `Githubリポジトリ検索結果: ${data.search.repositoryCount} ${repositoryUnit}`;
+          return <h2>{title}</h2>;
         }}
       </Query>
     </ApolloProvider>
